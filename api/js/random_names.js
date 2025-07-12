ba52146dfea26f9d53781778e6fb3327121c07c3
RFVIJN0 <RFVIJN0@outlook.com>
2025-07-12 19:59:29 +0800

    add random_names

---
+++ api/js/random_names.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+async function getName() {
+    res = await axios({
+        'method': 'GET',
+        'url': 'https://www.mocklib.com/mock/random/name'
+    })
+    return res.data.name
+}
+
+const p = document.body.appendChild('p')
+p.innerText = getName()
